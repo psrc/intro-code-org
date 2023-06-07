f6dc3c33d62df1f9c2b3c878676fec3178abd493
cyyllam <cyyllam@gmail.com>
2023-06-07 16:08:53 -0700

    added scripts

---
+++ loops.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(here)
+
+file_01 <- read_csv(here('data', '050', 'Table9.csv'))
+file_02 <- read_csv(here('data', '050', 'Table10.csv'))
+file_03 <- read_csv(here('data', '050', 'Table11.csv'))

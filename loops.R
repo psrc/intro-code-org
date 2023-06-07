f8c275bea0f2ecc19113280d2aee2988b8e630ee
cyyllam <cyyllam@gmail.com>
2023-06-07 16:28:07 -0700

    added list to loops

--- loops.R
+++ loops.R
@@ -4,3 +4,5 @@ library(here)
 file_01 <- read_csv(here('data', '050', 'Table9.csv'))
 file_02 <- read_csv(here('data', '050', 'Table10.csv'))
 file_03 <- read_csv(here('data', '050', 'Table11.csv'))
+
+l <- list(file_01, file_02, file_03)
